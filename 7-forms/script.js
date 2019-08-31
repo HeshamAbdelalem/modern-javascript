bf68aac73e106e920ef1d8bd40440d06ffccc497
HeshamAbdelalem <hesham.abdelalem1@gmail.com>
2019-08-31 23:05:06 +0200

    feat: adds update

---
+++ 7-forms/script.js
@@ -0,0 +1,7 @@
+const form = document.querySelector('.signup-form');
+const username = document.querySelector('#username');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  console.log('submited');
+});
